6ce7eabed5035f7aa9d60a4c4da249b8125dfdd2
Namahar <45704980+Namahar@users.noreply.github.com>
2021-06-08 14:42:27 -0400

    initial commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/formData', methods=['POST'])
+def formData():
+
+   # get form data in dictionary
+   # index with url key
+   url = request.form['url']
+   print(url)
+
+   # make youtube-dl command
+   command = 'python3 youtube_dl/__main__.py --default-search "auto" -x --audio-format "mp3" --audio-quality 0 ' + url
+
+   os.system(command)
+
+   # return to index page with proper url route
+   return redirect('/', code=302)
+
+# render html
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   # run flask
+   app.run(debug=True, port=8000)
